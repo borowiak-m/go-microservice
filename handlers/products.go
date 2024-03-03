faf14731e98a1900f4b27e5cdcdcc4c07d7d9428
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-03-03 22:45:01 +0000

    Added getProduct func

--- handlers/products.go
+++ handlers/products.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 
@@ -17,13 +16,29 @@ func NewProducts(log *log.Logger) *Products {
 }
 
 func (prods *Products) ServeHTTP(reqW http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		prods.getProducts(reqW, req)
+		return
+	}
+	if req.Method == http.MethodPost {
+		prods.addProduct(reqW, req)
+		return
+	}
+
+	// catch all - for all other methods
+	reqW.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (prods *Products) getProducts(reqW http.ResponseWriter, req *http.Request) {
 	// get products
 	lp := data.GetProducts()
 	// encode to JSON format
-	dat, err := json.Marshal(lp)
+	err := lp.ToJSON(reqW)
 	if err != nil {
 		http.Error(reqW, "Unable to marshall products to json", http.StatusInternalServerError)
 	}
+}
 
-	reqW.Write(dat)
+func (prods *Products) addProduct(reqW http.ResponseWriter, req *http.Request) {
+	log.Println("POST request response")
 }

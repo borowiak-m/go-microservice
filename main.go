d7dffef2c2c757808c0291484bea02bf475c2ce6
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-03-03 18:51:32 +0000

    Added greeting handler

--- main.go
+++ main.go
@@ -11,12 +11,20 @@ import (
 func main() {
 	// new logger
 	newlogger := log.New(os.Stdout, "product-api", log.LstdFlags)
-	// create handler for root with new logger
+
+	// HANDLERS
+	//   create handler for root with new logger
 	handlerRoot := handlers.NewRoot(newlogger)
-	// new serve mux
+	//   create handler for greetings with logger
+	handlerGreeting := handlers.NewGreeting(newlogger)
+
+	// SERVER
+	//   new serve mux
 	servMx := http.NewServeMux()
-	// register handler as server for "/" pattern
+	//   register handlerRoot as server for "/" pattern
 	servMx.Handle("/", handlerRoot)
-	// create web server on port 9090 using defined serv mux
+	//   register handlerGreeting as server for "/greeting" pattern
+	servMx.Handle("/greeting", handlerGreeting)
+	//   create web server on port 9090 using defined serv mux
 	http.ListenAndServe(":9090", servMx)
 }

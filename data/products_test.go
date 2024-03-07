5e3e8d520198b77762864599fdf60f786b2481b5
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-03-07 00:56:10 +0000

    Added more unit tests for prod validation

--- data/products_test.go
+++ data/products_test.go
@@ -1,15 +1,68 @@
 package data
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
-func TestCheckValidation(t *testing.T) {
+func TestProduct_InvalidSKUReturnsErr(t *testing.T) {
 	prod := &Product{
 		Name:  "nics",
 		Price: 90,
-		SKU:   "2AAA-DD-A",
+		SKU:   "2AAA",
 	}
 	err := prod.Validate()
 
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestProduct_MissingNameReturnsErr(t *testing.T) {
+	prod := Product{
+		Price: 1.22,
+	}
+
+	err := prod.Validate()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestProduct_IncorrectPriceReturnsErr(t *testing.T) {
+	prod := Product{
+		Name:  "Dsfdsf",
+		Price: -1.22,
+	}
+
+	err := prod.Validate()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestProduct_ValidProductSuccess(t *testing.T) {
+	prod := &Product{
+		Name:  "Namelyname",
+		Price: 90,
+		SKU:   "2AAA-GGG-DDD",
+	}
+	err := prod.Validate()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestProduct_ProductToJSONSuccess(t *testing.T) {
+	prods := []*Product{
+		{Name: "abc"},
+	}
+
+	byts := bytes.NewBufferString("")
+	err := ToJSON(prods, byts)
 	if err != nil {
 		t.Fatal(err)
 	}

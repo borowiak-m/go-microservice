f21ee31dbcb1d25a508309e92f878b4c34e87157
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-03-11 22:19:28 +0000

    Refactored to own files

--- data/json.go
+++ data/json.go
@@ -8,13 +8,13 @@ import (
 // Serializes contents of the collection to JSON
 // NewEncoder has better performance than json.Unmarshall
 // due to no usage of interim buffer in memory
-func ToJSON(prod interface{}, wrt io.Writer) error {
+func ToJSON(obj interface{}, wrt io.Writer) error {
 	encoder := json.NewEncoder(wrt)
-	return encoder.Encode(prod)
+	return encoder.Encode(obj)
 }
 
 // Decoding struct from JSON
-func FromJSON(prod interface{}, re io.Reader) error {
+func FromJSON(obj interface{}, re io.Reader) error {
 	decoder := json.NewDecoder(re)
-	return decoder.Decode(prod)
+	return decoder.Decode(obj)
 }

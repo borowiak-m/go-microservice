515c22ca374f3b555986a19190b86338b8972b3a
Mariusz Borowiak <142698842+borowiak-m@users.noreply.github.com>
2024-03-13 22:33:34 +0000

    Added comments

--- handlers/product_middleware.go
+++ handlers/product_middleware.go
@@ -11,17 +11,19 @@ import (
 // docs: https://github.com/gorilla/mux?tab=readme-ov-file#middleware
 func (prods *Products) MiddlewareProductValidation(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(respW http.ResponseWriter, req *http.Request) {
+		// define empty user object
 		prod := data.Product{}
-
+		// deserialize json from request body into the empty object
 		err := data.FromJSON(&prod, req.Body)
+		// handler errors
 		if err != nil {
 			prods.log.Println("[ERROR] deserializing Product", err)
 			respW.WriteHeader(http.StatusBadRequest)
 			data.ToJSON(&GenericError{Message: err.Error()}, respW)
 			return
 		}
-
 		// validate the product
+		// capture validation errors
 		vErrs := prods.val.Validate(prod)
 		if vErrs != nil {
 			prods.log.Println("[ERROR] validating product", err)
@@ -29,10 +31,10 @@ func (prods *Products) MiddlewareProductValidation(next http.Handler) http.Handl
 			data.ToJSON(&ValidationError{Messages: vErrs.Errors()}, respW)
 			return
 		}
-
+		// add user obj to conext and add to request
 		ctx := context.WithValue(req.Context(), KeyProduct{}, prod)
 		req = req.WithContext(ctx)
-
+		// pass to next handler
 		next.ServeHTTP(respW, req)
 	})
 }
--- handlers/product_post.go
+++ handlers/product_post.go
@@ -8,7 +8,7 @@ import (
 
 // POST request function to handle a creation of a new product
 func (prods *Products) CreateProduct(reqW http.ResponseWriter, req *http.Request) {
-	prods.log.Println("POST request response")
+	prods.log.Println("POST product request response")
 	prod := req.Context().Value(KeyProduct{}).(data.Product)
 	prods.log.Println("[DEBUG] inserting prod:", prod)
 	data.AddProduct(&prod)
